d721c64356189150c418ff9519bb4127b71bcf11
youming.tang <youmin.tang@elestyle.jp>
2025-10-30 16:07:20 +0900

    test: verify Husky formatting works

---
+++ test-format.ts
@@ -0,0 +1,9 @@
+// Test file for Husky formatting
+const badlyFormatted={name:"test",value:123};
+const anotherBadFormat   =   {   test  :  "value"  }  ;
+
+function   poorlyFormatted(  a:number  ,  b:string  )  {
+return   a  +  b  ;
+}
+
+export   {   badlyFormatted  ,  anotherBadFormat  ,  poorlyFormatted  }  ;
